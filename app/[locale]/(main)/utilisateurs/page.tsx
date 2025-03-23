3bfcb1ac975ec7aafc34322812a9a13623aba94e
Mohamed Amine MEBARKI <mohamed.mebarki@UNODIS.DZ>
2025-03-23 22:21:23 +0100

    update

--- app/[locale]/(main)/utilisateurs/page.tsx
+++ app/[locale]/(main)/utilisateurs/page.tsx
@@ -7,33 +7,12 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import UserTypeSwitch from "@/components/UserTypeSwitch";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import { Link } from "@/i18n/routing";
 import { getTranslations } from "next-intl/server";
 import MyPagination from "@/components/navigation/MyPagination";
-import TabSearch from "@/components/TabSearch";
-import { LIST_NEGOCIATEURS_QUERY, LIST_USERS_QUERY } from "@/graphql/queries";
-import { User } from "@/lib/interfaces";
-import UserFilter from "@/components/UserFilter";
-import StatusFilter from "@/components/StatusFilter";
 import UsersTableSkeleton from "@/components/UsersTableSkeleton";
-import { fetchGraphQL } from "@/app/actions/fetchGraphQL";
-
-import { getServerSession } from "next-auth";
-import auth from "@/auth";
 import { Suspense } from "react";
-import LogOutAgent from "@/components/LogOutAgent";
-import RateLimitReached from "@/components/RateLimitReached";
-
-interface NegociateurGraphQLResponse {
-  listUsers: {
-    id: string;
-    fullname: string;
-  }[];
-}
-
-interface GetUsersResponse {
-  listUsers: User[];
-}
 
 const page = async (props: {
   searchParams?: Promise<{
@@ -41,73 +20,107 @@ const page = async (props: {
     page?: string;
     userType?: string;
     status?: string;
-  }>;
+  }>[];
 }) => {
   const t = await getTranslations("clients");
-  const session = await getServerSession(auth);
-  const userRole = session?.user?.roleid;
-  const searchParams = await props.searchParams;
-  const currentPage = Number(searchParams?.page) || 0;
-  const searchquery = searchParams?.searchquery || "";
-  const roleid = Number(searchParams?.userType) || 1;
-  const status =
-    Number(searchParams?.status) === 0 || Number(searchParams?.status) === 1
-      ? Number(searchParams?.status)
-      : 1;
-  const roleSwitcher = roleid;
-  const take = 6;
 
-  const getRoleIdFilter = () => {
-    if (status === 1) {
-      return userRole === 3 ? roleSwitcher : 1;
-    } else if (status === 0) {
-      return 0;
-    }
-    return {};
-  };
-  //comment
-  const roleIdFilter = getRoleIdFilter();
-  let users;
-  try {
-    users = await fetchGraphQL<GetUsersResponse>(
-      LIST_USERS_QUERY,
+  // Données fictives en dur
+  const users = {
+    listUsers: [
       {
-        skip: currentPage,
-        take,
-        searchquery,
-        roleid: roleIdFilter,
-        ...(userRole === 2 && { negotiatorid: session?.user.negotiatorId }),
+        id: "1",
+        fullname: "salime Olaoluwah",
+        phonenumber: "090090497395",
+        email: "salime@finetude.com",
+        poste: "CEO",
+        clientAffecte: "FINNETUDE",
+        validation: "validateur 2",
+        status: "Verified",
       },
       {
-        headers: {
-          "Cache-Control": "no-cache",
-        },
-      }
-    );
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    if (error === "Token is revoked") {
-      return <LogOutAgent />;
-    }
-  }
-  let negociateurs;
-  try {
-    negociateurs = await fetchGraphQL<NegociateurGraphQLResponse>(
-      LIST_NEGOCIATEURS_QUERY
-    );
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    negociateurs = null;
-  }
+        id: "2",
+        fullname: "chahin oda",
+        phonenumber: "090090497395",
+        email: "chahin@slikpis.com",
+        poste: "DIRECTEUR",
+        clientAffecte: "SLIK PIS",
+        validation: "validateur 2",
+        status: "Not Verified",
+      },
+      {
+        id: "3",
+        fullname: "houda slimi",
+        phonenumber: "090090497395",
+        email: "houda@bessa.com",
+        poste: "GERENT",
+        clientAffecte: "BESSA",
+        validation: "validateur 2",
+        status: "Verified",
+      },
+      {
+        id: "4",
+        fullname: "sami bikachy",
+        phonenumber: "090090497395",
+        email: "sami@finlab.com",
+        poste: "DFC",
+        clientAffecte: "FINLAB",
+        validation: "validateur1",
+        status: "Verified",
+      },
+      {
+        id: "5",
+        fullname: "Adekeye Olaoluwah",
+        phonenumber: "090090497395",
+        email: "adekeye@bessa.com",
+        poste: "DC",
+        clientAffecte: "BESSA",
+        validation: "validateur1",
+        status: "Verified",
+      },
+      {
+        id: "6",
+        fullname: "Nihad Olaoluwah",
+        phonenumber: "090090497395",
+        email: "nihad@bessa.com",
+        poste: "Operateur",
+        clientAffecte: "BESSA",
+        validation: "Initiateur",
+        status: "Verified",
+      },
+      {
+        id: "7",
+        fullname: "Esma wachdi",
+        phonenumber: "090090497395",
+        email: "esma@bessa.com",
+        poste: "Operateur",
+        clientAffecte: "BESSA",
+        validation: "Consultation",
+        status: "Verified",
+      },
+      {
+        id: "8",
+        fullname: "Yanis Toubi",
+        phonenumber: "090090497395",
+        email: "yanis@bessa.com",
+        poste: "Propriétaire",
+        clientAffecte: "Yanis toubi",
+        validation: "Consultation",
+        status: "Verified",
+      },
+    ],
+  };
+
   const tableHeaders = [
-    t("nom"),
-    t("type"),
-    t("email"),
-    t("telephone"),
-    ...(roleIdFilter === 1 ? [t("negociateur")] : []),
+    t("NomClient"),
+    t("NTelephoneClient"),
+    t("PosteClient"),
+    t("ClientAffecteClient"),
+    t("ValidationClient"),
+    t("StatusClient"),
   ];
+
   return (
-    <div className=" motion-preset-focus motion-duration-2000">
+    <div className="motion-preset-focus motion-duration-2000">
       <div className="mt-3"></div>
       <div className="flex items-center mt-16 mb-8 ml-8 gap-3">
         <div className="text-3xl font-bold text-primary md:text-left text-center">
@@ -117,19 +130,8 @@ const page = async (props: {
       </div>
 
       <div className="border border-gray-100 rounded-md p-4 mt-10">
-        <div className="flex justify-center gap-4 items-center md:justify-between flex-col md:flex-row">
-          {userRole === 3 && <UserFilter />}
-          {status === 2 && <div className="md:w-72"></div>}
-          <StatusFilter />
-          <div className="w-full md:w-fit">
-            <TabSearch />
-          </div>
-        </div>
         <div className="my-4">
-          <Suspense
-            key={`users-${currentPage}`}
-            fallback={<UsersTableSkeleton />}
-          >
+          <Suspense key={`users-0`} fallback={<UsersTableSkeleton />}>
             <Table>
               <TableHeader>
                 <TableRow>
@@ -140,37 +142,50 @@ const page = async (props: {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {users &&
-                  users?.listUsers.map((user: User) => (
-                    <TableRow key={user.id}>
-                      <TableCell>{user.fullname}</TableCell>
-                      <TableCell className={"capitalize"}>
-                        {user.followsbusiness
-                          ? t("entreprise")
-                          : t("particulier")}
-                      </TableCell>
-                      <TableCell>{user.email}</TableCell>
-                      <TableCell>{user.phonenumber}</TableCell>
-                      {user.roleid === 1 && (
-                        <TableCell>
-                          {
-                            negociateurs?.listUsers.find(
-                              (negociateur) =>
-                                negociateur.id === user.negotiatorid
-                            )?.fullname
-                          }
-                        </TableCell>
-                      )}
-                      <TableCell className="flex justify-center items-center">
-                        <Link
-                          href={`/utilisateurs/${user.id}`}
-                          className=" rounded-md bg-primary py-1 px-4 text-white hover:bg-primary/80 shadow"
-                        >
-                          {t("plus")}
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                {users?.listUsers.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>{user.fullname}</TableCell>
+                    <TableCell>{user.phonenumber}</TableCell>
+                    <TableCell>{user.poste}</TableCell>
+                    <TableCell>{user.clientAffecte}</TableCell>
+                    <TableCell
+                      className={`${
+                        user.validation === "validateur 2"
+                          ? "text-green-500"
+                          : user.validation === "validateur1"
+                          ? "text-orange-500"
+                          : user.validation === "Initiateur"
+                          ? "text-gray-500"
+                          : "text-red-500"
+                      }`}
+                    >
+                      {user.validation}
+                    </TableCell>
+                    <TableCell
+                      className={`${
+                        user.status === "Verified"
+                          ? "text-[#15383E]"
+                          : "text-[#D71F1F]"
+                      }`}
+                    >
+                      <div
+                        className={`${
+                          user.status === "Verified"
+                            ? "bg-[#227844] bg-opacity-20"
+                            : "bg-[#D71F1F] bg-opacity-20"
+                        } px-1 rounded-md py-1 text-center`}
+                      >
+                        {user.status}
+                      </div>
+                    </TableCell>
+
+                    <TableCell className="flex justify-center items-center">
+                      <Link href={`/utilisateurs/${user.id}`}>
+                        <BsThreeDotsVertical />
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
                 {users?.listUsers.length === 0 && (
                   <TableRow>
                     <TableCell colSpan={7} className="text-center">
